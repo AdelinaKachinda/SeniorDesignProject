f90ef32642f268a5c2da99c1e34b40b7527d0be3
Adelina Kachinda <96156467+AdelinaKachinda@users.noreply.github.com>
2024-12-14 11:56:39 -0600

    Added basic FaceDetection code

--- FaceDetection/FaceDetectionBasics.py
+++ FaceDetection/FaceDetectionBasics.py
@@ -2,9 +2,9 @@ import cv2
 import mediapipe as mp
 import time 
 
-cap = cv2.VideoCapture("Video/FaceDetectionVideo.mp4")
+cap = cv2.VideoCapture("FaceDetection/Videos/FaceDetectionVideo.mp4")
 
 while True:
     success, image = cap.read()
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", image)
     cv2.waitKey(1)
